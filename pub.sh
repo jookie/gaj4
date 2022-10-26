96566767454e38cc4106b4de0e3ccd78acdf2db2
dovy <dovpeles@gmail.com>
2022-10-26 14:59:35 -0400

    fix:readme

--- pub.sh
+++ pub.sh
@@ -16,20 +16,4 @@ git push
 # # 👇️ get package version
 # yarn --version
 
-# make sure path matches with npm config get prefix
-# export PATH=/usr/local/share/npm/bin:$PATH
-# heroku git:remote -a <your-app-name> 
-###
-    # mkdir demo && cd demo
-
-    # # install from NPM
-    # npm install rtcmulticonnection
-
-    # # or clone from github
-    # git clone https://github.com/muaz-khan/RTCMultiConnection.git ./
-
-    # # install all required packages
-    # # you can optionally include --save-dev
-   
-   
 
